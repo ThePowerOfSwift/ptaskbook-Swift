83453c489496f6c458b4abc79f09df440c694980
Mihail Șalari <info@mihailsalari.com>
2016-08-09 22:02:50 +0300

    Started from Beginner, resolved 1...4

---
+++ ptaskbook.playground/Pages/Begin.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Begin1
+// Дана сторона квадрата a. Найти его периметр P = 4·a.
+/*
+ let squareSide = 3.1
+ let perimeter = Double(4) * squareSide
+ */
+
+// Begin2
+// Дана сторона квадрата a. Найти его площадь S = a2.
+/*
+ let squareSide = 13.1
+ let squareAria = squareSide * squareSide
+ */
+
+// Begin3
+// Даны стороны прямоугольника a и b. Найти его площадь S = a·b и периметр P = 2·(a + b)
+/*
+ let triangeSideA = 13
+ let triangeSideB = 12
+ let triangleAria = triangeSideA * triangeSideB
+ let trianglePerimeter = 2 * (triangeSideA + triangeSideB)
+ */
+
+// Begin4
+// Дан диаметр окружности d. Найти ее длину L = π·d. В качестве значения π использовать 3.14.
+/*
+ let circleDiameter = 130
+ let pi = 3.14
+ let length = pi * Double(circleDiameter)
+ */
